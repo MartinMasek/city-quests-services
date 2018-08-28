c8d1c4a7a2e8b343499955e517a5b39d90e4f3d4
MartinMasek <martin@Martins-MacBook-Pro.local>
2018-08-28 22:24:16 +0200

    added express

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const http = require('http')
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express();
 
-const handler = (req, res) => {
-    console.log("Running server");
-    res.end("Hello world from github!");
-}
-
-const server = http.createServer(handler);
-
-server.listen(port, err => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(`Listening on port: ${port}`);
-    }
+app.get('/', function (req, res) {
+    res.send("<b>Hello</b>")
 })
+
+app.get('/welcome', function (req, res) {
+    res.send('<b>Hello</b> welcome to my http server made with express');
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+app.listen(3000,function(){
+    console.log("Listening")
+})
+// const http = require('http')
+// const port = process.env.PORT || 3000;
+
+// const handler = (req, res) => {
+//     console.log("Running server");
+//     res.end("Hello world from github!");
+// }
+
+// const server = http.createServer(handler);
+
+// server.listen(port, err => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(`Listening on port: ${port}`);
+//     }
+// })
