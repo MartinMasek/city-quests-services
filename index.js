95e6178f64b73ed9e7f05d67bbe327483551774c
MartinMasek <martin@Martins-MacBook-Pro.local>
2018-08-28 18:20:08 +0200

    new msg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log("Running server");
-    res.end("Hello world!");
+    res.end("Hello world from github!");
 }
 
 const server = http.createServer(handler);
