b5410e43ca0acd08cc3b5be013faaa8ccefbeb27
MartinMasek <martin@Martins-MacBook-Pro.local>
2018-09-02 11:32:35 +0200

    get all adventures endpoint

--- index.js
+++ index.js
@@ -7,9 +7,47 @@ app.get('/', function (req, res) {
     console.log("LOG")
 })
 
-app.get('/welcome', function (req, res) {
-    res.send('<b>Hello</b> welcome to my http server made with express');
+app.get('/welcome', (req, res) => {
+    res.send('<b>Hello</b> welcome to my http server made with ddexpress');
 });
+
+app.get('/store/get-all-adventures', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        adventures: [
+            {
+                id: 1,
+                title: "Stop the golem",
+                subtitle: "The ancient golem woke up and 2 people are missing",
+                description: "Description description description description description description description description description description description description",
+                price: 14.95,
+                currency: "EUR",
+                bought: true,
+                active: true,
+            },
+            {
+                id: 2,
+                title: "Where is Amy?",
+                subtitle: "Who is Amy? Where is she?",
+                description: "Description about how Amyh got lost. It's a secret figure that. ...",
+                price: 9.95,
+                currency: "EUR",
+                bought: false,
+                active: false
+            },
+            {
+                id: 3,
+                title: "Blanic knights",
+                subtitle: "Will the blanic knights help to save Czechia in its darkest hour?",
+                description: "Knights Description description description description description description description description description description description description",
+                price: 4.95,
+                currency: "EUR",
+                bought: false,
+                active: false
+            }
+        ]
+    }))
+})
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
@@ -18,20 +56,3 @@ app.use(function (req, res, next) {
 app.listen(port, function () {
     console.log(`Listening on ${port}`)
 })
-// const http = require('http'
-
-// const handler = (req, res) => {
-//     console.log("Running server");
-//     res.end("Hello world from github!");
-// }
-
-// const server = http.createServer(handler);
-
-// server.listen(port, err => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log(`Listening on port: ${port}`);
-//     }
-// })
